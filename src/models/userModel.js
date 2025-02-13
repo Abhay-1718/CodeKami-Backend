0723444b83aad1298d58de42b0d1f50d39f96074
Abhay-1718 <abhaysingh80766@gmail.com>
2025-02-13 10:21:18 +0530

    create model for users

---
+++ src/models/userModel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: function () {
+      
+      return !this.googleId;
+    },
+  },
+  googleId: {
+    type: String,
+    unique: true,
+    sparse: true, 
+  },
+  verifyOtp: {
+    type: String,
+    default: "",
+  },
+  verifyOtpExpireAt: {
+    type: Number,
+    default: 0,
+  },
+  isAccountVerified: {
+    type: Boolean,
+    default: false,
+  },
+  resetOtp: {
+    type: String,
+    default: "",
+  },
+  resetOtpExpireAt: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
