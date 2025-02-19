943bc82a7ab7494b9f521a8daedc1f47620a79d2
Abhay-1718 <abhaysingh80766@gmail.com>
2025-02-19 10:06:18 +0530

    add frontend origin in app.js

--- src/app.js
+++ src/app.js
@@ -15,11 +15,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: '*', // Allow all origins for testing
+    origin: 'http://localhost:5173', // Allow only the frontend's origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,  // Allow cookies and credentials to be sent
 };
+
 
 app.use(cors(corsOptions));  // Use the CORS middleware
 app.use(cookieParser());
